53b424f0b285605031511383d73b30db0b742b04
Ismael Oliveira <ismaoliveira25@gmail.com>
2026-02-03 00:44:37 +0000

    Servidor criado com rotas testes

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+ 
+
+const app = express()
+
+ 
+
+app.get('/', (req, res) => {
+  
+ res.send('Hello World')
+
+})
+ app.get('/oi', (req, res) => {
+
+    res.send('olá, mundoo!!')
+
+
+})
+
+ 
+
+app.listen(3000, () => {
+
+  console.log('Server is running on http://localhost:3000')
+
+})
