4180ff78594b65f340cb687e30a9a229c1d7ec3c
Ismael Oliveira <ismaoliveira25@gmail.com>
2026-02-09 23:30:56 +0000

    Refatorar código e adicionar arquivos de requisição HTTP para endpoints "lista" e "personagens"

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 import express from 'express'
 
- 
-
 const app = express()
 
- 
-
 app.get('/', (req, res) => {
-  
- res.send('Hello World')
-
-})
- app.get('/oi', (req, res) => {
-
-    res.send('olá, mundoo!!')
-
-
+  res.send('Hello World')
 })
 
- 
+app.get('/oi', (req, res) => {
+  res.send('olá, mundoo!!')
+})
+
+const lista = [
+  'Rick Sanchez',
+  'Morty Smith',
+  'Summer Smith',
+  'Beth Smith',
+  'Jerry Smith'
+]
+
+app.get('/lista', (req, res) => {
+  res.send(personagens)
+})
+app.get('/personagens/:id', (req, res) => {
+  const id= req.params.id
+
+  const personagem = lista[id - 1]
+
+  res.send(personagem)
+})
 
 app.listen(3000, () => {
-
   console.log('Server is running on http://localhost:3000')
-
 })
